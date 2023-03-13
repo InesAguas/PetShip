ab2f15c45957222a3a56a73ce123492bbcaea52a
Steph <stephanesq1@gmail.com>
2023-03-13 15:58:08 +0000

    Click Teste
    Click

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import 'bootstrap/dist/js/bootstrap.js'
 import jquery from 'jquery'
 import popper from 'popper.js'
 
+
+
 window.$ = jquery
 window.jQuery = jquery
 window.Popper = popper
+
 
 createApp(App).mount('#app')
