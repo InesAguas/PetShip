082389774b0c43ddd8a4344703b9291de5db890a
InesAguas <inocas_99@hotmail.com>
2023-03-13 14:46:06 +0000

    teste

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import jquery from 'jquery'
+import popper from 'popper.js'
+
+window.$ = jquery
+window.jQuery = jquery
+window.Popper = popper
 
 createApp(App).mount('#app')
