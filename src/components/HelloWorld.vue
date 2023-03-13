668e6babf98568c315cc31e556fd1b65ec2cdd7d
InesAguas <inocas_99@hotmail.com>
2023-03-13 19:48:34 +0000

    teste api

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,10 +2,10 @@
   <div class="col-4">
     <form>
       <div class="mb-3">
-        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="nome">
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="utilizador.nome">
       </div>
       <div class="mb-3">
-        <input type="text" class="form-control" id="exampleInputPassword1" v-model="apelido">
+        <input type="text" class="form-control" id="exampleInputPassword1" v-model="utilizador.apelido">
       </div>
       <button type="button" class="btn btn-primary" v-on:click="adicionar">Submit</button>
     </form>
@@ -13,28 +13,39 @@
 </template>
 
 <script>
-import { tSMethodSignature } from '@babel/types';
 import axios from 'axios'
 export default {
   name: 'HelloWorld',
   data() {
     return {
-      nome: "",
-      apelido: ""
+      utilizador: {
+        nome: "",
+        apelido: ""
+      }
+      
     }
   },
   methods: {
     adicionar() {
-      console.log(this.nome);
-      console.log(this.apelido);
 
-      this.$http.post('https://api.example.com/data')
+     /* axios({
+      method: 'post',
+      url: 'http://127.0.0.1:8000/api/utilizadores',
+      data: {
+        nome: this.nome,
+        apelido: this.apelido
+      }
+    });*/
+
+      axios.post("http://127.0.0.1:8000/api/utilizadores", this.utilizador);
+
+      /*this.$http.post('https://api.example.com/data')
         .then(response => {
           console.log(response.data)
         })
         .catch(error => {
           console.log(error)
-        })
+        })*/
 
     }
 
