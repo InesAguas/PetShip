0f15090fb388519fcdc3afa608fd85079782a5ba
InesAguas <inocas_99@hotmail.com>
2023-03-20 11:25:31 +0000

    Alertas de erros
    Alertas de campos vazios, login errado, etc

--- src/components/FormRegisto.vue
+++ src/components/FormRegisto.vue
@@ -6,6 +6,9 @@
                 <div class="card-body p-4 text-center">
                     <img src="../assets/logo_sem_fundo.png" alt="Logo" width="150" height="150" class="">
                     <h2 class="fw-bold text-uppercase mb-4" style="color: #653208;">Registar</h2>
+                    <div class="alert alert-danger" role="alert" style="padding:5px" v-show="erro">
+                        {{ this.mensagemErro }}
+                    </div>
                     <div class="mb-3">
                         <input type="text" class="form-control" placeholder="Nome" v-model="utilizador.nome">
                     </div>
@@ -41,11 +44,13 @@ export default {
     data() {
         return {
             utilizador: {
-                nome: "",
-                email: "",
-                password: "",
+                nome: null,
+                email: null,
+                password: null,
                 tipo: 0
-            }
+            },
+            erro: false,
+            mensagemErro: null
 
         }
     },
@@ -54,9 +59,32 @@ export default {
     },
     methods: {
         registar() {
+
+            if (this.utilizador.nome == null || this.utilizador.email == null || this.utilizador.password == null) {
+                this.mensagemErro = "Não podem existir campos vazios."
+                this.erro = true;
+                return;
+            }
+
+            if (this.utilizador.tipo != 1 && this.utilizador.tipo != 2) {
+                this.mensagemErro = "Selecione um tipo de conta."
+                this.erro = true;
+                return;
+            }
+
+
             this.axios.post("/registar", this.utilizador)
                 .then(function (response) {
                     console.log(response);
+                    //aqui ou diz que o registo foi feito e existe a verificaçao de email
+                    //ou entao faz diretamente login...
+                })
+                .catch((error) => {
+                    if (error.response.status == 422) {
+                        //aqui temos de modificar a mensagem de erro, talvez meter a mensagem que o servidor envia???
+                        this.mensagemErro = "????"
+                        this.erro = true;
+                    }
                 });
         }
     }
