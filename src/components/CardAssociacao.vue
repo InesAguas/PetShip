c4c1cc234ab5831f83925a7a145e34ffaacbf439
Steph <stephanesq1@gmail.com>
2023-04-01 11:56:02 +0100

    CardAssociacao

---
+++ src/components/CardAssociacao.vue
@@ -0,0 +1,15 @@
+<template>
+      <div class="card" style="width: 18rem">
+      <img src="../assets/transferir.png" class="card-img-top" alt="..." height="" />
+      <div class="card-body">
+            <h5 class="card-title fw-bold text-center" style="color: #653208">2Mãos4Patas</h5>
+      </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'CardAssociacao'
+}
+
+
+</script>
