3d731bfbc4e217c35628a73f04bd138953eb975e
InesAguas <inocas_99@hotmail.com>
2023-04-04 15:22:10 +0100

    Imagens dinamicas

--- src/components/associacoes/CardAssociacao.vue
+++ src/components/associacoes/CardAssociacao.vue
@@ -1,12 +1,13 @@
 <template>
       <div class="card" style="width: 18rem">
-      <img src="../../assets/transferir.png" class="card-img-top" alt="..." height="180" />
+      <img :src="require('../../assets/associacoes/' + associacao.fotografia)" class="card-img-top" alt="..." height="180" />
       <div class="card-body">
             <h5 class="card-title fw-bold text-center" style="color: #653208">{{associacao.nome}}</h5>
       </div>
     </div>
 </template>
 <script>
+
 export default {
     name: 'CardAssociacao',
     props: [
