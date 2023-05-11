e1e895326150bc2d2fe28e80460947fc9d876b06
InesAguas <inocas_99@hotmail.com>
2023-05-11 16:10:15 +0100

    modal criado

--- src/components/dashboard/GestaoAnimais.vue
+++ src/components/dashboard/GestaoAnimais.vue
@@ -1,6 +1,51 @@
 <template>
     <NavBar></NavBar>
+    <div class="container ms-0">
+        <div class="row flex-nowrap">
     <PainelDashboard></PainelDashboard>
+    <div class="col">
+        <div class="row my-3">
+            <div class="col">
+                <h2 class="fw-bold" style="color: #653208;">Animais</h2>
+            </div>
+            <div class="col">
+            <button type="button" class="btn text-white fw-bold float-end" style="background-color: #FD7E14;" data-bs-toggle="modal" data-bs-target="#formRegisto">Adicionar</button>
+        </div>
+        </div>
+        
+        <table class="table">
+            <thead>
+                <tr>
+                  <th>Nome</th>
+                  <th>First</th>
+                  <th>Last</th>
+                  <th>Handle</th>
+                </tr>
+              </thead>
+        </table>
+    </div>
+</div>
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="formRegisto" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Registar Animal</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import NavBar from '../NavBar.vue';
--- src/components/dashboard/PainelDashboard.vue
+++ src/components/dashboard/PainelDashboard.vue
@@ -1,6 +1,5 @@
 <template>
-    <div class="container-fluid">
-        <div class="row flex-nowrap">
+        
             <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-white">
                 <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                     <a href="/dashboard" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
@@ -60,8 +59,7 @@
                     <hr>
                 </div>
             </div>
-        </div>
-    </div>
+        
 </template>
 <script>
 
