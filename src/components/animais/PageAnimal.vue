87fc731cc3d386e5953478481b0386505a70d3ed
Steph <stephanesq1@gmail.com>
2023-05-17 22:38:15 +0100

    Form candidatura ( Modal)

--- src/components/animais/PageAnimal.vue
+++ src/components/animais/PageAnimal.vue
@@ -1,4 +1,5 @@
 <template>
+    <ModalCandidatura v-if="showModal" :utilizadorLogado="utilizadorLogado" ></ModalCandidatura>
     <NavBar></NavBar>
     <div class="container mt-5">
         <div class="row">
@@ -10,17 +11,16 @@
             <div id="carouselExample" class="carousel slide">
                 <div class="carousel-inner" v-if="animal.fotografias">
                     <div class="carousel-item active">
-                        <img :src="animal.fotografias.length == 0 ? require('../../assets/default_animal.png') : animal.fotografias[0] "
+                        <img :src="animal.fotografias.length == 0 ? require('../../assets/default_animal.png') : animal.fotografias[0]"
                             class="d-block w-100" height="400" style="object-fit:scale-down;" alt="...">
                     </div>
                     <div class="carousel-item" v-for="(imagem) in animal.fotografias.slice(1)" :key="imagem">
-                        <img :src=imagem
-                            class="d-block w-100" height="400" style="object-fit:scale-down;" alt="...">
+                        <img :src=imagem class="d-block w-100" height="400" style="object-fit:scale-down;" alt="...">
                     </div>
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon"  aria-hidden="true" ></span>
-                    <span class="visually-hidden" >Previous</span>
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
                 </button>
                 <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
@@ -73,14 +73,17 @@
                                     src="../../assets/facebook.png" height="30px"></a>
                             <a href="https://www.instagram.com/" class="me-1 mt-4"><img class="mt-3"
                                     src="../../assets/instagram.png" height="30px"></a>
-                            <button @click="mandarMensagem" type="button" class="btn mt-3 ms-2" style="background-color:#FD7E14; color:white"><svg
-                                    xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white"
-                                    class="bi bi-chat-dots me-2" viewBox="0 0 16 16">
+                            <button @click="mandarMensagem" type="button" class="btn mt-3 ms-2"
+                                style="background-color:#FD7E14; color:white"><svg xmlns="http://www.w3.org/2000/svg"
+                                    width="20" height="20" fill="white" class="bi bi-chat-dots me-2" viewBox="0 0 16 16">
                                     <path
                                         d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
                                     <path
                                         d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2z" />
                                 </svg>Enviar mensagem</button>
+                            <button @click="adotar" type="button" class="btn mt-3 ms-2"
+                                style="background-color:#FD7E14; color:white" data-bs-toggle="modal" data-bs-target="#exampleModal">Adotar</button>
+                                
                         </div>
                     </div>
                     <div class="row">
@@ -93,16 +96,16 @@
                     <div class="text-start">
                         <h4 class="fw-bold">{{ $t('paginaAnimalMsg.carateristicas') }}</h4>
                         <p class="fw-bold fs-6">
-                            {{ $t('formAnimalMsg.especie') }}: <span class="fw-normal">{{animal.especie}}</span><br>
-                            {{ $t('formAnimalMsg.raca') }}: <span class="fw-normal">{{animal.raca}}</span><br>
-                            {{ $t('formAnimalMsg.sexo') }}: <span class="fw-normal">{{animal.sexo}}</span><br>
-                            {{ $t('paginaAnimalMsg.tamanho') }}: <span class="fw-normal">{{animal.porte}}</span><br>
-                            {{ $t('formAnimalMsg.idade') }}: <span class="fw-normal">{{animal.idade}}</span><br>
-                            {{ $t('formAnimalMsg.cor') }}: <span class="fw-normal">{{animal.cor}}</span><br>
+                            {{ $t('formAnimalMsg.especie') }}: <span class="fw-normal">{{ animal.especie }}</span><br>
+                            {{ $t('formAnimalMsg.raca') }}: <span class="fw-normal">{{ animal.raca }}</span><br>
+                            {{ $t('formAnimalMsg.sexo') }}: <span class="fw-normal">{{ animal.sexo }}</span><br>
+                            {{ $t('paginaAnimalMsg.tamanho') }}: <span class="fw-normal">{{ animal.porte }}</span><br>
+                            {{ $t('formAnimalMsg.idade') }}: <span class="fw-normal">{{ animal.idade }}</span><br>
+                            {{ $t('formAnimalMsg.cor') }}: <span class="fw-normal">{{ animal.cor }}</span><br>
                         </p>
                         <h4 class="fw-bold">{{ $t('formAnimalMsg.descricao') }}</h4>
                         <p>
-                            {{animal.descricao}}
+                            {{ animal.descricao }}
                         </p>
                     </div>
                 </div>
@@ -113,27 +116,31 @@
 <style>
 .carousel-control-prev-icon,
 .carousel-control-next-icon {
-  background-color: #FD7E14;
+    background-color: #FD7E14;
 }
-
 </style>
 <script>
 import NavBar from '../NavBar.vue'
+import ModalCandidatura from './ModalCandidatura.vue'
 export default {
 
     name: 'PagePetSitting',
     components: {
         NavBar,
+        ModalCandidatura,
 
     },
     data() {
         return {
             animal: [],
             utilizador: [],
+            showModal: false,
+            utilizadorLogado: null,
         }
 
     },
     mounted() {
+        this.utilizadorLogado = JSON.parse(localStorage.getItem('utilizador'));
         this.axios.get('animal/' + this.$route.params.id)
             .then(response => {
                 this.animal = response.data.animal
@@ -147,12 +154,15 @@ export default {
     methods: {
         mandarMensagem() {
             this.$router.push({
-            name: "mensagens", //use name for router push
-            params: {
-                id: this.utilizador.id,
-                nome: this.utilizador.nome,
-            }
+                name: "mensagens", //use name for router push
+                params: {
+                    id: this.utilizador.id,
+                    nome: this.utilizador.nome,
+                }
             });
+        },
+        adotar() {
+            this.showModal = true
         }
     }
 }
