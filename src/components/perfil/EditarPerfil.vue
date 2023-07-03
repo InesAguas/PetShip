bc7502a05aef09f7036bb3923025b7c045caae16
InesAguas <inocas_99@hotmail.com>
2023-07-03 13:27:54 +0100

    perfil na dashboard

--- src/components/perfil/EditarPerfil.vue
+++ src/components/perfil/EditarPerfil.vue
@@ -1,10 +1,20 @@
 <template>
+    <NavBar></NavBar>
     <div v-if="utilizador">
-        <NavBar></NavBar>
-        <EditarPerfilParticular v-if="utilizador.tipo == 1 " :utilizador="utilizador"></EditarPerfilParticular>
-        <EditarPerfilAssociacao v-if="utilizador.tipo == 2"  :utilizador="utilizador"></EditarPerfilAssociacao>
+        <div class="container-fluid ms-0">
+            <div class="row flex-nowrap">
+                <PainelDashboard :isActive="'perfil'"></PainelDashboard>
+                <div class="col-8" style="height:500px;">
+                    <div v-if="utilizador">
+                        <EditarPerfilParticular v-if="utilizador.tipo == 1 " :utilizador="utilizador"></EditarPerfilParticular>
+                        <EditarPerfilAssociacao v-if="utilizador.tipo == 2"  :utilizador="utilizador"></EditarPerfilAssociacao>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
     </div>
-    
 
 </template>
     
@@ -12,13 +22,15 @@
 import NavBar from '../NavBar.vue';
 import EditarPerfilParticular from './EditarPerfilParticular.vue';
 import EditarPerfilAssociacao from './EditarPerfilAssociacao.vue';
+import PainelDashboard from '../dashboard/PainelDashboard.vue';
 
 export default {
     name: 'EditarPerfil',
     components: {
         NavBar,
         EditarPerfilParticular,
-        EditarPerfilAssociacao
+        EditarPerfilAssociacao,
+        PainelDashboard,
     },
     data() {
         return {
--- src/components/perfil/PagePerfil.vue
+++ src/components/perfil/PagePerfil.vue
@@ -1,22 +1,35 @@
 <template>
     <NavBar></NavBar>
-    <div v-if="utilizador">
-    <PerfilParticular v-if="utilizador.tipo == 1" :utilizador="utilizador" :utilizadorLogado="utilizadorLogado"></PerfilParticular>
-    <PerfilAssociacao v-if="utilizador.tipo == 2" :utilizador="utilizador" :utilizadorLogado="utilizadorLogado"></PerfilAssociacao>
-</div>
+    
+        <div class="container-fluid ms-0">
+            <div class="row flex-nowrap">
+                <PainelDashboard :isActive="'perfil'"></PainelDashboard>
+                <div class="col-8" style="height:500px;">
+                    <div v-if="utilizador">
+                    <PerfilParticular v-if="utilizador.tipo == 1" :utilizador="utilizador"
+                        :utilizadorLogado="utilizadorLogado"></PerfilParticular>
+                    <PerfilAssociacao v-if="utilizador.tipo == 2" :utilizador="utilizador"
+                        :utilizadorLogado="utilizadorLogado"></PerfilAssociacao>
+                    </div>
+                </div>
+            </div>
+        </div>
 </template>
 <script>
 
 import NavBar from '../NavBar.vue';
 import PerfilParticular from './PerfilParticular.vue';
 import PerfilAssociacao from './PerfilAssociacao.vue';
+import PainelDashboard from '../dashboard/PainelDashboard.vue';
+
 
 export default {
     name: 'PagePerfil',
     components: {
         NavBar,
         PerfilParticular,
-        PerfilAssociacao
+        PerfilAssociacao,
+        PainelDashboard,
     },
     data() {
         return {
@@ -28,17 +41,17 @@ export default {
         this.utilizadorLogado = JSON.parse(sessionStorage.getItem('utilizador'));
         console.log(this.utilizadorLogado);
 
-        if(this.$route.params.id == null) {
+        if (this.$route.params.id == null) {
             this.utilizador = this.utilizadorLogado;
             console.log(this.utilizador);
         } else {
             this.axios.get('/perfil/' + this.$route.params.id)
-        .then(response => {
-            this.utilizador = response.data.utilizador;
-        })
-        .catch(() => {
-           this.$router.push({ name: 'notFound' });
-        })
+                .then(response => {
+                    this.utilizador = response.data.utilizador;
+                })
+                .catch(() => {
+                    this.$router.push({ name: 'notFound' });
+                })
         }
     }
 }
